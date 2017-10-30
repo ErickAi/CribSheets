d14eaa675c4f200b261d2c1f69c4c77c858b5af6
Erick <e.aytkulov@gmail.com>
2017-10-30 18:46:32 +0300

    local date and time api

---
+++ JavaExamples/0 veni vidi vici.java
@@ -0,0 +1,44 @@
+break, continue, return
+extends, implements
+instanceof
+
+//ИТЕРАТОР ДЛЯ Map
+for (Map.Entry entry : properties.entrySet()){
+    System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
+        }
+Iterator<Integer> iterator = set.iterator();//итератор для удаления
+while (iterator.hasNext()){
+    //если значение больше 10, то удаляем и переходим к следующему
+    if (iterator.next()>10) iterator.remove();
+}	
+
+//ЛОГИЧЕСКИЕ ОПЕРАТОРЫ
+&&	AND	(и) 	||	OR 	(или)		!	NOT	(не)
+return a < b ? a : b; //если a < b тру - вернуть а. иначе - b.	
+
+//Распространённые комбинации и выражения	
+m && !m			false
+m || !m			true
+! (a && b)		!a || !b
+! (a || b)		!a && !b
+	 
+//Escape-исключения
+\t	Табуляция.
+\b	Клавишу Backspace.
+\n	Новую строку.
+\r	Возврат каретки.
+\f	Перевод формата.
+\'	Одинарной кавычки.
+\"	Двойной кавычки.
+\\	Наклонной черты влево.
+
+//String.format
+%s	String
+%d	целое число: int, long, …/td>
+%f	вещественное число: float, double
+%b	boolean
+%c	char
+%t	Date
+%%	Символ %
+
+"%3$s %4$s %2$s %1$s", "four", "three", "one", "two"
